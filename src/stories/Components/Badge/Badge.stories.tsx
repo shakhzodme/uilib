6dbb7518488c80d5742ff9ece5a8262461e44b4b
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2023-12-11 16:09:18 +0500

    feat: badges

--- src/stories/Components/Badge/Badge.stories.tsx
+++ src/stories/Components/Badge/Badge.stories.tsx
@@ -1,10 +1,16 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import Component from "./Badge";
+import Component, { BadgeProps, storyBadgeVariants } from "./Badge";
+import { General } from "untitledui-js";
 
 const meta = {
   title: "Components/Badge",
   component: Component,
+  argTypes: {
+    outline: {
+      type: "boolean",
+    },
+  },
 } satisfies Meta<typeof Component>;
 
 export default meta;
@@ -13,5 +19,95 @@ type Story = StoryObj<typeof meta>;
 export const Playground: Story = {
   args: {
     children: "Badge",
+    outline: false,
+    color: "gray",
+    variant: "pill",
+    size: "md",
   },
+};
+
+const RenderVariants: React.FC<{
+  color: (typeof storyBadgeVariants)["color"][number];
+}> = ({ color }) => {
+  return (
+    <div className="flex flex-col gap-4">
+      {storyBadgeVariants.outline.map((outline) =>
+        storyBadgeVariants.variant.map((variant) =>
+          (
+            [
+              {},
+              { leftIcon: <General.Placeholder /> },
+              { rightIcon: <General.Placeholder /> },
+              {
+                leftIcon: <General.Placeholder />,
+                rightIcon: <General.Placeholder />,
+              },
+            ] as BadgeProps[]
+          ).map((props) => (
+            <div className="flex gap-4 items-start">
+              {storyBadgeVariants.size.map((size) => (
+                <Component
+                  variant={variant}
+                  color={color}
+                  size={size}
+                  outline={outline}
+                  {...props}
+                >
+                  Badge
+                </Component>
+              ))}
+            </div>
+          )),
+        ),
+      )}
+    </div>
+  );
+};
+
+export const Gray: Story = {
+  render: () => <RenderVariants color="gray" />,
+};
+
+export const Brand: Story = {
+  render: () => <RenderVariants color="brand" />,
+};
+
+export const Error: Story = {
+  render: () => <RenderVariants color="error" />,
+};
+
+export const Warning: Story = {
+  render: () => <RenderVariants color="warning" />,
+};
+
+export const Success: Story = {
+  render: () => <RenderVariants color="success" />,
+};
+
+export const GrayBlue: Story = {
+  render: () => <RenderVariants color="gray-blue" />,
+};
+
+export const BlueLight: Story = {
+  render: () => <RenderVariants color="blue-light" />,
+};
+
+export const Blue: Story = {
+  render: () => <RenderVariants color="blue" />,
+};
+
+export const Indigo: Story = {
+  render: () => <RenderVariants color="indigo" />,
+};
+
+export const Purple: Story = {
+  render: () => <RenderVariants color="purple" />,
+};
+
+export const Pink: Story = {
+  render: () => <RenderVariants color="pink" />,
+};
+
+export const Orange: Story = {
+  render: () => <RenderVariants color="orange" />,
 };
--- src/stories/Components/Badge/Badge.tsx
+++ src/stories/Components/Badge/Badge.tsx
@@ -1,46 +1,129 @@
 import cn from "@/utils/cn";
+import updateProps from "@/utils/update-props";
 import { VariantProps, cva } from "cva";
 import React from "react";
 
-export const badgeVariants = cva("inline-flex border", {
-  variants: {
-    variant: {
-      pill: ["rounded-full"],
-      badge: [],
+export const badgeVariants = cva(
+  "inline-flex items-center justify-center border",
+  {
+    variants: {
+      variant: {
+        pill: "rounded-full",
+        badge: "",
+      },
+      outline: {
+        true: [],
+        false: [],
+      },
+      color: {
+        gray: "text-gray-700",
+        brand: "text-brand-700",
+        error: "text-error-700",
+        warning: "text-warning-700",
+        success: "text-success-700",
+        "gray-blue": "text-gray-blue-700",
+        "blue-light": "text-blue-light-700",
+        blue: "text-blue-700",
+        indigo: "text-indigo-700",
+        purple: "text-purple-700",
+        pink: "text-pink-700",
+        orange: "text-orange-700",
+      },
+      size: {
+        sm: ["py-0.5 px-2 text-xs font-medium gap-0.5"],
+        md: ["py-0.5 px-2.5 text-sm font-medium gap-1"],
+        lg: ["py-1 px-3 text-sm font-medium gap-1"],
+      },
     },
-    outline: {
-      true: [],
-      false: [],
-    },
-    color: {
-      gray: ["bg-gray-50 text-gray-700 border-gray-200"],
-      brand: ["bg-brand-50 text-brand-700 border-brand-200"],
-      error: ["bg-error-50 text-error-700 border-error-200"],
-      warning: ["bg-warning-50 text-warning-700 border-warning-200"],
-      success: ["bg-success-50 text-success-700 border-success-200"],
-      "gray-blue": ["bg-gray-blue-50 text-gray-blue-700 border-gray-blue-200"],
-      "blue-light": [
-        "bg-blue-light-50 text-blue-light-700 border-blue-light-200",
-      ],
-      blue: ["bg-blue-50 text-blue-700 border-blue-200"],
-      indigo: ["bg-indigo-50 text-indigo-700 border-indigo-200"],
-      purple: ["bg-purple-50 text-purple-700 border-purple-200"],
-      pink: ["bg-pink-50 text-pink-700 border-pink-200"],
-      orange: ["bg-orange-50 text-orange-700 border-orange-200"],
-    },
-    size: {
-      sm: ["py-0.5 px-2 text-xs font-medium"],
-      md: ["py-0.5 px-2.5 text-sm font-medium"],
-      lg: ["py-1 px-3 text-sm font-medium"],
+    compoundVariants: [
+      {
+        outline: false,
+        color: "gray",
+        className: "bg-gray-50 border-gray-200",
+      },
+      { outline: true, color: "gray", className: "border-gray-600" },
+      {
+        outline: false,
+        color: "brand",
+        className: "bg-brand-50 border-brand-200",
+      },
+      { outline: true, color: "brand", className: "border-brand-600" },
+      {
+        outline: false,
+        color: "error",
+        className: "bg-error-50 border-error-200",
+      },
+      { outline: true, color: "error", className: "border-error-600" },
+      {
+        outline: false,
+        color: "warning",
+        className: "bg-warning-50 border-warning-200",
+      },
+      { outline: true, color: "warning", className: "border-warning-600" },
+      {
+        outline: false,
+        color: "success",
+        className: "bg-success-50 border-success-200",
+      },
+      { outline: true, color: "success", className: "border-success-600" },
+      {
+        outline: false,
+        color: "gray-blue",
+        className: "bg-gray-blue-50 border-gray-blue-200",
+      },
+      { outline: true, color: "gray-blue", className: "border-gray-blue-600" },
+      {
+        outline: false,
+        color: "blue-light",
+        className: "bg-blue-light-50 border-blue-light-200",
+      },
+      {
+        outline: true,
+        color: "blue-light",
+        className: "border-blue-light-600",
+      },
+      {
+        outline: false,
+        color: "blue",
+        className: "bg-blue-50 border-blue-200",
+      },
+      { outline: true, color: "blue", className: "border-blue-600" },
+      {
+        outline: false,
+        color: "indigo",
+        className: "bg-indigo-50 border-indigo-200",
+      },
+      { outline: true, color: "indigo", className: "border-indigo-600" },
+      {
+        outline: false,
+        color: "purple",
+        className: "bg-purple-50 border-purple-200",
+      },
+      { outline: true, color: "purple", className: "border-purple-600" },
+      {
+        outline: false,
+        color: "pink",
+        className: "bg-pink-50 border-pink-200",
+      },
+      { outline: true, color: "pink", className: "border-pink-600" },
+      {
+        outline: false,
+        color: "orange",
+        className: "bg-orange-50 border-orange-200",
+      },
+      { outline: true, color: "orange", className: "border-orange-600" },
+      { variant: "badge", size: "sm", className: "rounded-md" },
+      { variant: "badge", size: "md", className: "rounded-md" },
+      { variant: "badge", size: "lg", className: "rounded-lg" },
+    ],
+    defaultVariants: {
+      variant: "pill",
+      outline: false,
+      color: "gray",
+      size: "md",
     },
   },
-  defaultVariants: {
-    variant: "pill",
-    outline: false,
-    color: "gray",
-    size: "md",
-  },
-});
+);
 
 type BadgeVariants = VariantProps<typeof badgeVariants>;
 type StoryVariants = {
@@ -70,6 +153,8 @@ export const storyBadgeVariants: StoryVariants = {
 export interface BadgeProps extends BadgeVariants {
   className?: string;
   children: React.ReactNode;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
 }
 
 const Badge: React.FC<BadgeProps> = ({
@@ -79,15 +164,27 @@ const Badge: React.FC<BadgeProps> = ({
   outline,
   size,
   children,
+  leftIcon,
+  rightIcon,
 }) => {
   return (
     <div
       className={cn(
         badgeVariants({ variant, color, outline, size }),
+        { "pl-1.5": leftIcon && size == "sm" },
+        { "pr-1.5": rightIcon && size == "sm" },
+        { "pl-2": leftIcon && size == "md" },
+        { "pr-2": rightIcon && size == "md" },
+        { "pl-2.5": leftIcon && size == "lg" },
+        { "pr-2.5": rightIcon && size == "lg" },
         className,
       )}
     >
+      {leftIcon &&
+        updateProps(leftIcon, { stroke: "currentColor", size: "12px" })}
       {children}
+      {rightIcon &&
+        updateProps(rightIcon, { stroke: "currentColor", size: "12px" })}
     </div>
   );
 };
