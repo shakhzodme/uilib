b760dab108e0eeed1b13bcb77c9e12833c567841
Phoenixifier <phoenixbboy@gmail.com>
2023-12-15 17:51:42 +0500

    fix: adaptive footer first

--- src/stories/WebsiteComponents/Footers/Large/First/First.tsx
+++ src/stories/WebsiteComponents/Footers/Large/First/First.tsx
@@ -10,17 +10,17 @@ const First: React.FC<{
 }> = ({ navigation }) => {
   return (
     <div className="container mx-auto flex flex-col gap-16 pt-16 pb-12">
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-6 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8">
         {navigation.map((value, index) => (
-          <div key={index} className="flex flex-col gap-4">
+          <div key={index} className="flex flex-col justify-center items-center gap-4 xl:items-start">
             <p className="text-quarterary text-sm font-semibold">
               {value.title}
             </p>
-            <nav className="flex flex-col gap-3 items-start">
+            <nav className="flex flex-col gap-3 xl:items-start">
               {value.links.map((link, index) => (
                 <Button
                   key={index}
-                  className="p-0"
+                  className="p-0 text-center"
                   component="a"
                   variant="link-gray"
                   {...link}
@@ -32,7 +32,7 @@ const First: React.FC<{
       </div>
       <div className="flex flex-col gap-10">
         <div className="h-px w-full border-t border-secondary" />
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col items-center gap-5 justify-between md:flex-row md:gap-0">
           <h1 className="text-xl font-bold">Logo</h1>
           <p className="text-md text-quarterary">
             © 2077 Untitled UI. All rights reserved.
